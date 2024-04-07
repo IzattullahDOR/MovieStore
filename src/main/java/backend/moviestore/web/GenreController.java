bd14b7f21c1eab3b14a50714ad22b595f7abc4c4
izattullah <116265920+izattullah@users.noreply.github.com>
2024-04-07 21:33:48 +0300

    Added adding new genre functionality
    user can now add new genre

--- src/main/java/backend/moviestore/web/GenreController.java
+++ src/main/java/backend/moviestore/web/GenreController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import backend.moviestore.domain.Genre;
 import backend.moviestore.domain.GenreRepository;
 
 @Controller
@@ -18,6 +20,21 @@ public class GenreController {
 
         model.addAttribute("genres", gRepository.findAll());
         return "genrelist";
+    }
+
+    // handle add genre
+    @GetMapping("/addgenre")
+    public String addgenre(Model model) {
+
+        model.addAttribute("genre",new Genre());
+        return "/addgenre"; //addcategory.html
+    }
+
+    // handle Save new genre
+    @PostMapping("/savegenre")
+    public String save(Genre genre){
+        gRepository.save(genre);
+        return "redirect:genrelist";
     }
 
 
